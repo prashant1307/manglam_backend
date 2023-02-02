6b9df37df91935429efd7c7086c3b246d4361055
prashant1307 <prashantlorgev8@gmail.com>
2023-02-02 16:44:21 +0530

    server

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const cors=require("cors")
+require('dotenv').config();
 const app = express()
+const port=process.env.PORT
 app.use(cors())
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
 app.get('/',(req,res) => res.send('hello'))
 
-app.listen(8080,() => {console.log('server started on port 8080')})
+app.listen(port,() => {console.log('server started on port 8080')})

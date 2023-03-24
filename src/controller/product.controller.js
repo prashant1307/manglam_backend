818739e120d2b646a52504c1206ff40521ba518e
prashant1307 <prashantlorgev8@gmail.com>
2023-03-25 00:37:23 +0530

    products working fine

--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -2,45 +2,20 @@ const productModel = require("../models/product.model")
 
 const getProduct=async(limit,page,category,sort,low,high,strike)=>{
     let products=await productModel.find().limit(limit)
-    if(category){
-        products=await productModel.find({ancestor:category}).limit(limit)
-        if(low){
-            products
-        }
-    }
+   
     return products;
 }
 
-const getOneProduct=async(id)=>{
-    try {
-        let findData=await productModel.findOne({id})
-        return findData
-    } catch (e) {
-        return e+"hel"
-    }
-    // try{
-    //     let findData = await productModel.findOne({id});
-    //     console.log(typeof findData , findData)
-    //     if(findData.title == null ||findData.title == undefined ){
-    //         return {
-    //             status:false,
-    //             massage:'Something went wrong please try again later !'
-    //         }
-    //     }
-    //     else{
-    //         return {
-    //             status:true,
-    //             massage:'Product data fetched sucessfully',
-    //             data : findData
-    //         }
-    //     }
-    //   }
-    //   catch(e){
-    //     return {
-    //         status:false,
-    //         massage:e.message
-    //     }
-    // }
+const getOneProduct=async(req,res)=>{
+    const {id}=req.params
+try {
+    console.log(id)
+    let oneProd=await productModel.findOne({_id:id})
+   
+    res.send(oneProd)
+} catch (e) {
+    res.send(e)
+}
 }
 
 module.exports={getProduct,getOneProduct}
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -6,16 +6,14 @@ const { getProduct, getOneProduct } = require("../controller/product.controller"
 
 ProductRoute.use(cors())
 
+ProductRoute.get("/:id",getOneProduct)
+
 ProductRoute.get('/',async(req,res)=>{
     const {limit,page,category,sort,low,high,strike}=req.query;
     let data=await getProduct(limit,page,category,sort,low,high,strike)
     res.send(data)
 })
 
-ProductRoute.get("/:id",async(req,res)=>{
-    const id = req.params.id
-    let ans = await getOneProduct(id)
-    res.send(ans)
-})
+
 
 module.exports=ProductRoute
